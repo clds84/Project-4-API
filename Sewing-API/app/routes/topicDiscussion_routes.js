76a0759baeec388d3233c5efc8336983e3365f43
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2022-04-24 12:30:37 -0400

    update route working

--- Sewing-API/app/routes/topicDiscussion_routes.js
+++ Sewing-API/app/routes/topicDiscussion_routes.js
@@ -76,6 +76,7 @@ router.post('/topicDiscussion', requireToken, (req, res, next) => {
 // UPDATE
 // PATCH /topicDiscussions/5a7db6c74d55bc51bdf39793
 //
+//
 router.patch('/topicDiscussion/:id', requireToken, removeBlanks, (req, res, next) => {
 	// if the client attempts to change the `owner` property by including a new
 	// owner, prevent that by deleting that key/value pair
